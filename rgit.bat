4582a8146ca9cf07a78372c9f8f493bebdf6576e
tegueni <elyoussfiihaamza@gmail.com>
2024-05-28 23:07:38 +0100

    yy

--- rgit.bat
+++ rgit.bat
@@ -1,44 +1,49 @@
 @echo off
 setlocal
 
+rem Set the path to your Rust project's root directory
+
+
 rem Check if any arguments are provided
+if "%~1"=="" (
+    echo Error: No command provided. 2>nul
+    exit /b 1
+)
+
 
 
 rem Check if the first argument is a valid command
-if "%1"=="init" (
+if "%~1"=="init" (
     rem Run cargo with "init" command
-    start /b cargo run --bin init 2>nul
+    start /b cargo run --bin init 2>nul 
 
     rem Wait for the Rust command to finish
     :wait_init
-    tasklist | find /i "cargo.exe" 1>nul 2>nul
-    if errorlevel 1 (
-        rem If cargo is no longer running, set the hidden attribute for the .rgit directory and exit
-        attrib +h ".rgit" 2>nul
-        exit /b
-    )
-    rem If cargo is still running, wait for 1 second and check again
     timeout /t 1 /nobreak >nul
-    goto wait_init
+    tasklist | find /i "cargo.exe" 2>nul >nul
+    if not errorlevel 1 goto wait_init
 
-) else if "%1"=="add" (
+    rem If cargo is no longer running, set the hidden attribute for the .rgit directory and exit
+    attrib +h ".rgit" 2>nul
+    popd
+    exit /b
+) else if "%~1"=="add" (
     rem Run cargo with "add" command and passed arguments
-    start /b cargo run --bin add %*  2>nul
+    start /b cargo run --bin add %*
 
     rem Wait for the Rust command to finish
     :wait_add
-    tasklist | find /i "cargo.exe" 1>nul 2>nul
-    if errorlevel 1 (
-        rem If cargo is no longer running, set the hidden attribute for the .rgit directory and exit
-        attrib +h ".rgit" 1>nul 2>nul
-        exit /b
-    )
-    rem If cargo is still running, wait for 1 second and check again
-    timeout /t 1 /nobreak 1>nul 2>nul
-    goto wait_add
+    timeout /t 1 /nobreak >nul
+    tasklist | find /i "cargo.exe" >nul
+    if not errorlevel 1 goto wait_add
 
+    rem If cargo is no longer running, set the hidden attribute for the .rgit directory and exit
+    attrib +h ".rgit" 1>nul 2>nul
+    popd
+    exit /b
 ) else (
-    echo Usage: rgit <init|add> [arguments...]
+    echo Error: Unknown command "%~1"
+    popd
     exit /b 1
 )
 

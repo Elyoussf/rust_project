2af9880cef7e75003309dbabccf667d33693ff3b
tegueni <elyoussfiihaamza@gmail.com>
2024-06-01 01:12:45 +0100

    last version

--- Commit/main.rs
+++ Commit/main.rs
@@ -1,24 +1,21 @@
 use std::env;
 use std::fs::{self, File};
-// use std::io::{self, BufReader, BufWriter, Write};
-// use std::path::{Path, PathBuf};
-// use std::collections::HashSet;
 use sha1::{Sha1, Digest};
 use flate2::{Compression, write::ZlibEncoder};
 use chrono::Utc;
-use std::io ; 
+use std::io;
 use std::path::Path;
 use std::io::Write;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 || args[1] != "-m" {
-        eprintln!("Usage: rgit commit -m <message>");
+        println!("Usage: cargo run --bin commit -m <message>");
         return Ok(());
     }
 
     if !Path::new(".rgit").exists() {
-        eprintln!("Error: Repository not initialized. Please run 'rgit init' first.");
+        println!("Error: Repository not initialized. Please run 'cargo run --bin init' first.");
         return Ok(());
     }
 
@@ -34,26 +31,20 @@ fn create_file_with_content(dir_name: &Path, file_name: &str, content: &str) -> 
 }
 
 fn commit(message: &str) -> io::Result<()> {
-    // 1- Check the staging area
     let current_dir = env::current_dir()?;
     let rgit_dir = current_dir.join(".rgit");
 
-    // Assuming index file is used as a staging area
     let index_path = rgit_dir.join("index");
 
-    // If the index file does not exist or is empty, return an error
     if !index_path.exists() || is_file_empty(index_path.to_str().unwrap())? {
-        eprintln!("Nothing to commit. Staging area is empty.");
+        println!("Nothing to commit. Staging area is empty.");
         return Ok(());
     }
 
-    // 2- Save the message in the COMMIT_MSG
     create_file_with_content(&rgit_dir, "COMMIT_MSG", message)?;
 
-    // 3- Hash the content of the index and save it as a tree object
     let index_sha1 = hash_and_save_index()?;
 
-    // 4- Create the commit object
     let author_name = "Hamoudi";
     let author_email = "hamoudi@sbitar.com";
     let timestamp = Utc::now().to_rfc3339();
@@ -70,7 +61,6 @@ fn commit(message: &str) -> io::Result<()> {
     Ok(())
 }
 
-// To check if the file is empty (It's important for the index)
 fn is_file_empty(path: &str) -> Result<bool, std::io::Error> {
     let metadata = fs::metadata(path)?;
     Ok(metadata.len() == 0)
--- rgit.bat
+++ rgit.bat
@@ -2,15 +2,13 @@
 setlocal
 
 rem Set the path to your Rust project's root directory
-
+rem cd /d path\to\your\project
 
 rem Check if any arguments are provided
 if "%~1"=="" (
     echo Error: No command provided. 2>nul
     exit /b 1
 )
-
-
 
 rem Check if the first argument is a valid command
 if "%~1"=="init" (
@@ -29,13 +27,29 @@ if "%~1"=="init" (
     exit /b
 ) else if "%~1"=="add" (
     rem Run cargo with "add" command and passed arguments
-    start /b cargo run --bin add %*
+    start /b cargo run --bin add %* 2>nul
 
     rem Wait for the Rust command to finish
     :wait_add
     timeout /t 1 /nobreak >nul
     tasklist | find /i "cargo.exe" >nul
     if not errorlevel 1 goto wait_add
+
+    rem If cargo is no longer running, set the hidden attribute for the .rgit directory and exit
+    attrib +h ".rgit" 1>nul 2>nul
+    popd
+    exit /b
+) else if "%~1"=="commit" (
+    rem Shift the arguments to handle the double dash properly
+    shift
+    shift
+    start /b cargo run --bin %* 2>nul
+
+    rem Wait for the Rust command to finish
+    :wait_commit
+    timeout /t 1 /nobreak >nul
+    tasklist | find /i "cargo.exe" >nul
+    if not errorlevel 1 goto wait_commit
 
     rem If cargo is no longer running, set the hidden attribute for the .rgit directory and exit
     attrib +h ".rgit" 1>nul 2>nul
